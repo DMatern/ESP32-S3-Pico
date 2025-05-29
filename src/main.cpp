7817e4e12f623c5fc828d0ce8d063296909b68ec
Douglas <damaternprivate@matern.net>
2025-05-29 10:42:47 -0400

    Initial build

---
+++ src/main.cpp
@@ -0,0 +1,49 @@
+#include <Arduino.h>
+
+// ====================================
+// Flags
+
+uint8_t sysFlags = 0x00;
+// const byte sysFlag_newData = 0; // sysFlags bit 0:  0 = inactive	1 = active
+// const byte sysFlag_ = 1;  // sysFlags bit 1:  0 = inactive	1 = active
+// const byte sysFlag_ = 2;             // sysFlags bit 2:  0 = inactive	1 = active
+// const byte sysFlag_TareHold = 3; // sysFlags bit 3:  0 = inactive	1 = active
+// const byte sysFlag_Tare = 4;     // sysFlags bit 4:  0 = inactive	1 = active
+// const byte sysFlag_Select = 5;   // sysFlags bit 5:  0 = inactive	1 = active
+// const byte sysFlag_UP = 6;       // sysFlags bit 6:  0 = inactive	1 = active
+// const byte sysFlag_DN = 7;       // sysFlags bit 7:  0 = inactive	1 = active
+
+// ====================================
+// Global Variables
+
+// ====================================
+// Include Files
+
+#include <fastled.h>  //Onboard RGB LED Driver
+
+// ============================================================================
+// Function Declarations
+
+// ============================================================================
+// Setup
+// ============================================================================
+
+void setup() {
+
+}
+
+// ============================================================================
+// Loop
+// ============================================================================
+
+void loop() {
+
+}
+
+// ====================================
+// State Machine
+// ====================================
+
+// ====================================
+// Misc Functions
+// ====================================
